898019cd166e6c44f9f89b88f846d4f4e1870691
kongxiangyan <kcigaret@outlook.com>
2021-12-15 22:01:43 +0800

    docs: generate api docs

--- src/ts/functional/maybe.monad.ts
+++ src/ts/functional/maybe.monad.ts
@@ -10,20 +10,20 @@ import { invoker, curry } from './helpers'
  * ref: https://stackoverflow.com/questions/50512370/folktale-fantasyland-maybe-not-working-as-expected/57552439#57552439
  * comment: Just\Some
  */
-export class Maybe {
-  static of (a) {
+export class Maybe<V = any> {
+  static of <V>(a: V): Just<V> {
     return Maybe.just(a)
   }
 
-  static just (a) {
+  static just <V>(a: V): Just<V> {
     return new Just(a)
   }
 
-  static nothing () {
+  static nothing (): Nothing {
     return new Nothing()
   }
 
-  static fromNullable (a) {
+  static fromNullable <V>(a: V): Just<NonNullable<V>> | Nothing {
     return isNull(a) ? Maybe.nothing() : Maybe.just(a)
   }
 
@@ -35,7 +35,9 @@ export class Maybe {
     return isFalsy(a) ? Maybe.nothing() : Maybe.just(a)
   }
 
-  static fromNilable (a) {
+  static fromNilable (a: null | undefined): Nothing
+  static fromNilable <V>(a: V): Just<V>
+  static fromNilable (a: any): Just<any> | Nothing {
     return isNil(a) ? Maybe.nothing() : Maybe.just(a)
   }
 
@@ -43,50 +45,34 @@ export class Maybe {
     return isEmpty(a) ? Maybe.nothing() : Maybe.just(a)
   }
 
-  get isNothing () {
-    return false
-  }
-
-  get isJust () {
-    return false
-  }
-
-  get isMaybe () {
-    return true
-  }
+  get isMaybe (): true { return true }
 }
 
 // Derived class Just -> Presence of a value
 // Or named `Some` in some other monad implements
-export class Just<V> extends Maybe {
-  constructor (value) {
+export class Just<V> extends Maybe<V> {
+  _value: V
+
+  constructor (value: V) {
     super()
     this._value = value
   }
 
-  get value () {
-    return this._value
-  }
+  get value (): V { return this._value }
 
-  get isJust () {
-    return true
-  }
+  get isJust (): true { return true }
 
-  get isNothing () {
-    return false
-  }
+  get isNothing (): false { return false }
 
-  inspect () {
+  inspect (): string {
     return `Maybe.Just(${inspect(this._value)})`
   }
 
-  map (fn) {
+  map <R>(fn: (value: V) => R): Just<R> {
     return Maybe.just(fn(this._value))
   }
 
-  join () {
-    return this._value
-  }
+  join (): V { return this._value }
 
   chain (fn) {
     return fn(this._value)
@@ -115,37 +101,21 @@ export class Just<V> extends Maybe {
 
 // Derived class Empty -> Abscense of a value
 export class Nothing extends Maybe {
-  get value () {
-    throw new TypeError("Can't extract the value of a Nothing.")
-  }
+  get value (): void { throw new TypeError("Can't extract the value of a Nothing.") }
 
-  get isNothing () {
-    return true
-  }
+  get isNothing (): true { return true }
 
-  get isJust () {
-    return false
-  }
+  get isJust (): false { return false }
 
-  inspect () {
-    return 'Maybe.Nothing'
-  }
+  inspect (): string { return 'Maybe.Nothing' }
 
-  map (fn) {
-    return this
-  }
+  map (fn: (...args: any[]) => any): this { return this }
 
-  join () {
-    return this
-  }
+  join (): this { return this }
 
-  chain (fn) {
-    return this
-  }
+  chain (fn: (...args: any[]) => any): this { return this }
 
-  ap (mfn) {
-    return this
-  }
+  ap (mfn: (...args: any[]) => any): this { return this }
 
   orJust (dlft) {
     return Maybe.of(dlft)
@@ -155,9 +125,7 @@ export class Nothing extends Maybe {
     return maybeMonad
   }
 
-  filter () {
-    return this
-  }
+  filter (): this { return this }
 
   cata ({ Just, Nothing }) {
     return Nothing()
@@ -183,3 +151,20 @@ export const maybe = curry((v, f, m) => {
   }
   return f(m.value)
 })
+
+const add = (value: number): number => value + 1
+
+const square = (value: number): number => value ** 2
+
+const double = (value: number): number => value * 2
+
+const a = Maybe.of(1).map(add).map(square).map(double).map(String)
+// -> '8'
+
+const b = Maybe.nothing().map(add).map(square).map(double)
+// -> null
+
+const c = Maybe.fromNilable(null).map(add).map(square).map(double)
+// -> null
+
+console.log(a, b, c)
