daf5bb76fd3cd4087b73adee891637e6fbea98a9
kongxiangyan <kcigaret@outlook.com>
2021-12-20 00:59:38 +0800

    feat(types): add AnyStringRecord

--- src/ts/@types/object.ts
+++ src/ts/@types/object.ts
@@ -1,4 +1,6 @@
 
+export type AnyStringRecord = Record<string, any>
+
 export type UndefinedableByKeys<Target, Keys> = {
   [K in keyof Target]: K extends Keys ? Target[K] | undefined : Target[K]
 }
