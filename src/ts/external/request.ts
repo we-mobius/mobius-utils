3677beed7c193359b43ec770eeee42c24d250c73
kongxiangyan <kcigaret@outlook.com>
2021-12-20 01:06:05 +0800

    feat: rewrite request & response related functionalities

--- src/ts/external/request.ts
+++ src/ts/external/request.ts
@@ -1,37 +1,301 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import {
-  isPlainObject, asIs,
-  getPropByPath, deepMerge
+  isString, isPlainObject, isUndefined, deepMerge
 } from '../internal'
 import { flip } from '../functional'
-import { isSuccessResponse, errorToResponse } from './response'
-import { adaptMultiPlatformAsync } from './environment'
+import {
+  isResponse, isSuccessResponse, isFailResponse, isErrorResponse,
+  dataToResponse, errorToResponse
+} from './response'
+import {
+  isInWXMINAEnvironment, isInWebEnvironment, isInNodeEnvironment,
+  WXMINA_ENV_CONTEXTS,
+  adaptMultipleEnvironments
+} from './environment'
 
 import axios from 'axios'
 
-import type { WXMINAContexts } from './environment'
-import type { ResponseUnion } from './response'
+import type { AnyStringRecord } from '../@types/index'
+import type { ResponseUnion, ErrorResponse, ErrorResponseData } from './response'
 import type { AxiosRequestConfig } from 'axios'
 
-// reference: https://fetch.spec.whatwg.org/
-// reference: https://axios-http.com/
-// reference: https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+// @refer https://fetch.spec.whatwg.org/
+// @refer https://axios-http.com/
+//        https://github.com/axios/axios
+// @refer https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
 
-interface WXRequestResult {
-  data: any
+interface WXRequestResult<T = any> {
+  statusCode: number
+  data: T
 }
 
-interface BiuRequestConfig extends AxiosRequestConfig {
-  responseModifier: (response: ResponseUnion) => any
-  withDataExtracted: boolean
+export type responseTypeOfWXRequest = 'text' | 'arraybuffer'
+export type responseType = 'text' | 'json' | 'arraybuffer'
+export type dataType = 'json' | 'custom' | 'other'
+
+interface BiuOptions<SuccessData extends AnyStringRecord = AnyStringRecord, SendData = any> extends AxiosRequestConfig<SendData> {
+  url: string
+  data?: SendData
+  responseType?: responseType
+  dataType?: dataType
+  dataParser?: (data: any) => SuccessData
+  responseModifier?: (response: ResponseUnion<SuccessData>) => any
 }
 
-const commonConfig = {
+const DEFAULT_BIU_OPTIONS: Omit<BiuOptions, 'url'> = {
+  responseType: 'json',
+  dataType: 'json',
+  dataParser: JSON.parse,
   withCredentials: true,
-  withDataExtracted: false,
   headers: {
     'Content-Type': 'application/json'
+  }
+}
+
+/******************************************************************************************************
+ *
+ *                                                Biutor
+ *
+ ******************************************************************************************************/
+
+/**
+ *
+ */
+export class Biutor<
+  SuccessData extends AnyStringRecord = AnyStringRecord,
+  FailData extends AnyStringRecord = AnyStringRecord,
+  ErrorData extends ErrorResponseData = ErrorResponseData,
+  SendData = any
+> {
+  _options: BiuOptions<SuccessData, SendData>
+  _response: Promise<ResponseUnion<SuccessData, FailData, ErrorData>>
+
+  private readonly _resolve: (
+    value: ResponseUnion<SuccessData, FailData, ErrorData> | PromiseLike<ResponseUnion<SuccessData, FailData, ErrorData>>
+  ) => void
+
+  private readonly _reject: (reason?: any) => void
+
+  constructor (options: BiuOptions<SuccessData, SendData>) {
+    if (!isPlainObject(options)) {
+      throw (new TypeError('"options" is expected to be type of "PlainObject".'))
+    }
+    const preparedOptions = { ...DEFAULT_BIU_OPTIONS as BiuOptions<SuccessData, SendData>, ...options }
+    this._options = preparedOptions
+
+    let _resolve: (
+      value: ResponseUnion<SuccessData, FailData, ErrorData> | PromiseLike<ResponseUnion<SuccessData, FailData, ErrorData>>
+    ) => void
+    let _reject: (reason?: any) => void
+    this._resolve = value => _resolve(value)
+    this._reject = reason => _reject(reason)
+    this._response = new Promise((resolve, reject) => {
+      _resolve = resolve
+      _reject = reject
+    })
+  }
+
+  get options (): BiuOptions<SuccessData, SendData> { return this._options }
+
+  get response (): Promise<ResponseUnion<SuccessData>> { return this._response }
+  get modifiedResponse (): Promise<any> {
+    const { responseModifier } = this._options
+    if (isUndefined(responseModifier)) {
+      return this._response
+    } else {
+      return this._response.then(responseModifier)
+    }
+  }
+
+  get data (): Promise<any> {
+    return this._response.then(response => {
+      return response.data
+    })
+  }
+
+  get successData (): Promise<SuccessData> {
+    let _resolve: (value: SuccessData | PromiseLike<SuccessData>) => void
+    const promise = new Promise<SuccessData>((resolve, reject) => {
+      _resolve = resolve
+    })
+    void this._response.then(response => {
+      if (isSuccessResponse(response)) {
+        _resolve(response.data)
+      }
+    })
+    return promise
+  }
+
+  get failData (): Promise<FailData> {
+    let _resolve: (value: FailData | PromiseLike<FailData>) => void
+    const promise = new Promise<FailData>((resolve, reject) => {
+      _resolve = resolve
+    })
+    void this._response.then(response => {
+      if (isFailResponse(response)) {
+        _resolve(response.data)
+      }
+    })
+    return promise
+  }
+
+  get errorData (): Promise<ErrorData> {
+    let _resolve: (value: ErrorData | PromiseLike<ErrorData>) => void
+    const promise = new Promise<ErrorData>((resolve, reject) => {
+      _resolve = resolve
+    })
+    void this._response.then(response => {
+      if (isErrorResponse(response)) {
+        _resolve(response.data)
+      }
+    })
+    return promise
+  }
+
+  /**
+   * Set options by key.
+   */
+  setOptions <K extends keyof BiuOptions<SuccessData, SendData>>(
+    key: keyof BiuOptions<SuccessData, SendData>, value: BiuOptions<SuccessData, SendData>[K]
+  ): this {
+    this._options[key] = value
+    return this
+  }
+
+  /**
+   * @typeParam TD - The type of received data
+   * @typeParam SD - The type of data to send
+   */
+  static of <
+    SuccessData extends AnyStringRecord = AnyStringRecord,
+    FailData extends AnyStringRecord = AnyStringRecord,
+    ErrorData extends ErrorResponseData = ErrorResponseData,
+    SendData = any
+  >(
+    options: BiuOptions<SuccessData, SendData>
+  ): Biutor<SuccessData, FailData, ErrorData, SendData> {
+    return new Biutor(options)
+  }
+
+  sendWXMINARequest (): this {
+    if (!isInWXMINAEnvironment()) {
+      throw (new Error('WXMINA environment is not detected.'))
+    }
+    const { wxmina } = WXMINA_ENV_CONTEXTS
+
+    const { responseType } = this._options
+    let preparedResponseType: responseTypeOfWXRequest
+    if (!isUndefined(responseType) && ['text', 'arraybuffer'].includes(responseType)) {
+      preparedResponseType = responseType as responseTypeOfWXRequest
+    } else {
+      console.warn(`"${responseType!}" is not a valid responseType for "wx.request", fallback to "text" instead.`)
+      preparedResponseType = 'text'
+    }
+
+    void new Promise<WXRequestResult<ResponseUnion<SuccessData, FailData, ErrorData>>>((resolve, reject) => {
+      // @refer https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+      wxmina.request({
+        ...this._options,
+        responseType: preparedResponseType,
+        header: {
+          ...(this._options.headers ?? {})
+        },
+        success: resolve,
+        fail: reject
+      } as any)
+    }).then((response) => {
+      const { statusCode, data } = response
+      if (statusCode !== 200) {
+        throw (new Error(`Non 200 statusCode: ${response.statusCode}`))
+      }
+      return isResponse(data) ? data : dataToResponse(data)
+    }).catch(reason => {
+      return errorToResponse(reason) as ErrorResponse<ErrorData>
+    }).then((formattedResponse) => {
+      this._resolve(formattedResponse)
+    })
+
+    return this
+  }
+
+  sendWebRequest (): this {
+    if (!isInWebEnvironment()) {
+      throw (new Error('Web environment is not detected.'))
+    }
+    this.sendDefaultRequest()
+    return this
+  }
+
+  sendNodeRequest (): this {
+    if (!isInNodeEnvironment()) {
+      throw (new Error('Web environment is not detected.'))
+    }
+    this.sendDefaultRequest()
+    return this
+  }
+
+  sendDefaultRequest (): this {
+    const { dataType = 'json', dataParser = JSON.parse } = this._options
+
+    const parseData = (data: any): SuccessData => {
+      if (dataType === 'json' && isString(data)) {
+        try {
+          return dataParser(data)
+        } catch (error) {
+          throw (new TypeError('"data" is cannot be parsed to "JSON", please specify a different "dataType" in request options.'))
+        }
+      } else if (dataType === 'custom') {
+        try {
+          return dataParser(data)
+        } catch (error) {
+          throw (new TypeError('"data" of "custom" type is cannot be parsed, please specify a correct "dataParser" in request options.'))
+        }
+      } else {
+        return data
+      }
+    }
+
+    void axios.request({
+      ...this._options
+    }).then(axiosResponse => {
+      const { status, statusText, data } = axiosResponse
+      // @refer https://github.com/axios/axios#response-schema
+      if (status !== 200 && statusText !== 'OK') {
+        throw (new Error(`Non 200 statusCode: ${axiosResponse.status} - ${axiosResponse.statusText}`))
+      }
+
+      let formattedResponse: ResponseUnion<SuccessData, FailData, ErrorData>
+      if (isResponse<SuccessData, FailData, ErrorData>(data)) {
+        formattedResponse = data
+      } else if (isPlainObject(data)) {
+        formattedResponse = dataToResponse(data as SuccessData)
+      } else {
+        const parsedData = parseData(data)
+        if (isPlainObject(parsedData)) {
+          formattedResponse = dataToResponse(parsedData)
+        } else {
+          formattedResponse = dataToResponse({ value: parsedData } as unknown as SuccessData)
+        }
+      }
+
+      return formattedResponse
+    }).catch(reason => {
+      return errorToResponse(reason) as ErrorResponse<ErrorData>
+    }).then((formattedResponse) => {
+      this._resolve(formattedResponse)
+    })
+    return this
+  }
+
+  sendRequest (): this {
+    adaptMultipleEnvironments({
+      forWXMINA: this.sendWXMINARequest.bind(this),
+      forWeb: this.sendWebRequest.bind(this),
+      forNode: this.sendNodeRequest.bind(this),
+      forDefault: this.sendDefaultRequest.bind(this)
+    })
+    return this
   }
 }
 
@@ -42,64 +306,19 @@ const commonConfig = {
  ******************************************************************************************************/
 
 /**
- * - use wx.request for wechat miniapp environment
  * - use axios for web or node environment
- *
- * @param config request config
+ * - use wx.request for wechat miniapp environment
  */
-export const biu = async (config: BiuRequestConfig): Promise<any> => {
-  if (!isPlainObject(config)) {
+export const biu = async <SuccessData extends AnyStringRecord = AnyStringRecord, SendData extends AnyStringRecord = AnyStringRecord>(
+  options: BiuOptions<SuccessData, SendData>
+): Promise<ResponseUnion<SuccessData>> => {
+  if (!isPlainObject(options)) {
     throw (new TypeError('"config" is expected to be type of "object".'))
   }
-  config = { ...commonConfig, ...config }
+  const preparedOptions = { ...DEFAULT_BIU_OPTIONS as BiuOptions<SuccessData, SendData>, ...options }
+  const biutor = Biutor.of(preparedOptions)
 
-  return await adaptMultiPlatformAsync({
-    forWXMINA: async ({ wxmina }: WXMINAContexts): Promise<any> => {
-      return await new Promise<WXRequestResult>((resolve, reject) => {
-        wxmina.request({
-          dataType: 'json',
-          responseType: 'text',
-          ...config,
-          header: {
-            ...(config.headers ?? {})
-          },
-          success: resolve,
-          fail: reject
-        })
-      }).then((result) => {
-        return result.data
-      })
-    },
-    forDefault: async () => await axios({
-      ...config
-    }).then(response => {
-      if (response.status !== 200) {
-        throw (new Error(`${response.status}: ${response.statusText}`))
-      } else {
-        return response.data
-      }
-    })
-  })
-    .then((mobiusFlavorResponse) => {
-      if (!config.withDataExtracted) return mobiusFlavorResponse
-
-      // Mobius 的 API 规范中，success 响应的数据存在深层嵌套，如：
-      //  -> { status: 'success', data: { [REQUEST_TYPE]: actualData } }
-      if (isSuccessResponse(mobiusFlavorResponse)) {
-        // TODO: 适配多接口返回值捆绑的情况（far away | nerver）
-        const type: string = getPropByPath('data.payload.type', config)
-        const actualData = getPropByPath(`data.${type}`, mobiusFlavorResponse)
-        mobiusFlavorResponse.data = actualData
-        return { ...mobiusFlavorResponse, origin: mobiusFlavorResponse, data: actualData }
-      } else {
-        return mobiusFlavorResponse
-      }
-    })
-    .then(response => {
-      const responseModifier = config.responseModifier ?? asIs
-      return responseModifier(response)
-    })
-    .catch(errorToResponse)
+  return await biutor.sendRequest().modifiedResponse
 }
 
 export const modifyBiuConfig = flip(deepMerge)
@@ -108,8 +327,3 @@ export const asGetRequest = modifyBiuConfig({ method: 'GET' })
 export const withJSONContent = modifyBiuConfig({ headers: { 'Content-Type': 'application/json' } })
 export const withCredentials = modifyBiuConfig({ withCredentials: true })
 export const withoutCredentials = modifyBiuConfig({ withCredentials: false })
-
-/**
- * Remove redundant nestings of response data
- */
-export const withDataExtracted = modifyBiuConfig({ withDataExtracted: true })
--- src/ts/external/response.ts
+++ src/ts/external/response.ts
@@ -1,42 +1,53 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { isNumber, isString, isPlainObject } from '../internal/base'
+import { isNumber, isString, isPlainObject, isError } from '../internal/base'
 
-type AnyStringRecord = Record<string, any>
+import type { AnyStringRecord } from '../@types/index'
 
-export enum ResponseStatus {
-  success = 'success',
-  fail = 'fail',
-  error = 'error',
-  unknown = 'unknown'
-}
 export interface ResponseCode {
   code: number
   codeMessage: string
 }
+
+export enum ResponseStatus {
+  Success = 'success',
+  Fail = 'fail',
+  Error = 'error',
+  Unknown = 'unknown'
+}
+
+export interface ErrorResponseData<E extends Error = Error> extends AnyStringRecord {
+  error?: E
+}
+
 export interface BaseResponse<D extends AnyStringRecord = AnyStringRecord> extends ResponseCode {
   status: ResponseStatus
   statusMessage: string
   data: D
 }
 export interface SuccessResponse<D extends AnyStringRecord = AnyStringRecord> extends BaseResponse<D> {
-  status: ResponseStatus.success
+  status: ResponseStatus.Success
 }
 export interface FailResponse<D extends AnyStringRecord = AnyStringRecord> extends BaseResponse<D> {
-  status: ResponseStatus.fail
+  status: ResponseStatus.Fail
 }
-export interface ErrorResponse<D extends AnyStringRecord = AnyStringRecord> extends BaseResponse<D> {
-  status: ResponseStatus.error
+export interface ErrorResponse<D extends ErrorResponseData = ErrorResponseData> extends BaseResponse<D> {
+  status: ResponseStatus.Error
 }
 export interface UnknownResponse<D extends AnyStringRecord = AnyStringRecord> extends BaseResponse<D> {
-  status: ResponseStatus.unknown
+  status: ResponseStatus.Unknown
 }
-export type ResponseUnion<D extends AnyStringRecord = AnyStringRecord>
-  = SuccessResponse<D>
-  | FailResponse<D>
-  | ErrorResponse<D>
-  | UnknownResponse<D>
+export type ResponseUnion<
+  SD extends AnyStringRecord = AnyStringRecord,
+  FD extends AnyStringRecord = AnyStringRecord,
+  ED extends ErrorResponseData = ErrorResponseData
+>
+  = SuccessResponse<SD>
+  | FailResponse<FD>
+  | ErrorResponse<ED>
+  | UnknownResponse<AnyStringRecord>
+
 interface StatusSpecifiedResponse<D extends AnyStringRecord = AnyStringRecord> extends Omit<BaseResponse<D>, 'status'> {
   [key: string]: any
 }
@@ -49,25 +60,38 @@ export const DEFAULT_ERROR_RESPONSE_CODE: ResponseCode = { code: 2, codeMessage:
 export const DEFAULT_UNKNOWN_RESPONSE_CODE: ResponseCode = { code: 3, codeMessage: 'UNKNOWN_RESPONSE' }
 const DEFAULT_DATA: AnyStringRecord = {}
 
+/**
+ * Predicate whether the target is type of ResponseCode.
+ */
 export const isResponseCode = (tar: any): tar is ResponseCode =>
   isPlainObject(tar) && isNumber(tar.code) && isString(tar.codeMessage)
-export const isResponse = <D extends AnyStringRecord = AnyStringRecord>(tar: any): tar is ResponseUnion<D> =>
-  isPlainObject(tar) && tar.status in ResponseStatus && isString(tar.statusMessage) && isPlainObject(tar.data)
+/**
+ * @see {@link isResponseLike}
+ */
+export const isResponse = <
+  SD extends AnyStringRecord = AnyStringRecord, FD extends AnyStringRecord = AnyStringRecord,
+  ED extends ErrorResponseData = ErrorResponseData
+>(tar: any): tar is ResponseUnion<SD, FD, ED> =>
+    isPlainObject(tar) && Object.values(ResponseStatus).includes(tar.status) && isString(tar.statusMessage) && isPlainObject(tar.data)
+/**
+ * @see {@link isResponse}
+ */
 export const isResponseLike = (tar: any): boolean =>
   isPlainObject(tar) && isString(tar.status) && isString(tar.statusMessage) && isPlainObject(tar.data)
+
 export const isSuccessResponse = <D extends AnyStringRecord = AnyStringRecord>(tar: any): tar is SuccessResponse<D> =>
-  isResponse(tar) && tar.status === ResponseStatus.success
+  isResponse(tar) && tar.status === ResponseStatus.Success
 export const isFailResponse = <D extends AnyStringRecord = AnyStringRecord>(tar: any): tar is FailResponse<D> =>
-  isResponse(tar) && tar.status === ResponseStatus.fail
-export const isErrorResponse = <D extends AnyStringRecord = AnyStringRecord>(tar: any): tar is ErrorResponse<D> =>
-  isResponse(tar) && tar.status === ResponseStatus.error
+  isResponse(tar) && tar.status === ResponseStatus.Fail
+export const isErrorResponse = <D extends ErrorResponseData = ErrorResponseData>(tar: any): tar is ErrorResponse<D> =>
+  isResponse(tar) && tar.status === ResponseStatus.Error
 export const isUnknowResponse = <D extends AnyStringRecord = AnyStringRecord>(tar: any): tar is UnknownResponse<D> =>
-  isResponse(tar) && tar.status === ResponseStatus.unknown
+  isResponse(tar) && tar.status === ResponseStatus.Unknown
 
 export const makeSuccessResponse = <D extends AnyStringRecord = AnyStringRecord>(
   { statusMessage, code, codeMessage, data }: PartialStatusSpecifiedResponse<D>
 ): SuccessResponse<D> => ({
-    status: ResponseStatus.success,
+    status: ResponseStatus.Success,
     statusMessage: statusMessage ?? DEFAULT_STATUS_MESSAGE,
     code: code ?? DEFAULT_SUCCESS_RESPONSE_CODE.code,
     codeMessage: codeMessage ?? DEFAULT_SUCCESS_RESPONSE_CODE.codeMessage,
@@ -76,16 +100,16 @@ export const makeSuccessResponse = <D extends AnyStringRecord = AnyStringRecord>
 export const makeFailResponse = <D extends AnyStringRecord = AnyStringRecord>(
   { statusMessage, code, codeMessage, data }: PartialStatusSpecifiedResponse<D>
 ): FailResponse<D> => ({
-    status: ResponseStatus.fail,
+    status: ResponseStatus.Fail,
     statusMessage: statusMessage ?? DEFAULT_STATUS_MESSAGE,
     code: code ?? DEFAULT_FAIL_RESPONSE_CODE.code,
     codeMessage: codeMessage ?? DEFAULT_FAIL_RESPONSE_CODE.codeMessage,
     data: data ?? DEFAULT_DATA as D
   })
-export const makeErrorResponse = <D extends AnyStringRecord = AnyStringRecord>(
+export const makeErrorResponse = <D extends ErrorResponseData = ErrorResponseData>(
   { statusMessage, code, codeMessage, data }: PartialStatusSpecifiedResponse<D>
 ): ErrorResponse<D> => ({
-    status: ResponseStatus.error,
+    status: ResponseStatus.Error,
     statusMessage: statusMessage ?? DEFAULT_STATUS_MESSAGE,
     code: code ?? DEFAULT_ERROR_RESPONSE_CODE.code,
     codeMessage: codeMessage ?? DEFAULT_ERROR_RESPONSE_CODE.codeMessage,
@@ -96,7 +120,7 @@ export const makeUnknownResponse = <D extends AnyStringRecord = AnyStringRecord>
 ): UnknownResponse<D> => {
   console.warn(`[MobiusUtils][data] makeUnknownResponse: unknown response detected, ${JSON.stringify({ code, codeMessage, statusMessage, data })}`)
   return {
-    status: ResponseStatus.unknown,
+    status: ResponseStatus.Unknown,
     statusMessage: statusMessage ?? DEFAULT_STATUS_MESSAGE,
     code: code ?? DEFAULT_UNKNOWN_RESPONSE_CODE.code,
     codeMessage: codeMessage ?? DEFAULT_UNKNOWN_RESPONSE_CODE.codeMessage,
@@ -105,10 +129,16 @@ export const makeUnknownResponse = <D extends AnyStringRecord = AnyStringRecord>
 }
 
 export const dataToResponse = <D extends AnyStringRecord = AnyStringRecord>(data: D): SuccessResponse<D> => {
+  if (!isPlainObject(data)) {
+    throw (new TypeError('"data" is expected to be type of "PlainObject".'))
+  }
   return makeSuccessResponse({ data })
 }
-export const errorToResponse = <E extends Error>(error: E): ErrorResponse<{ error: E }> => {
-  const response = makeErrorResponse<{ error: E }>({})
+export const errorToResponse = <E extends Error>(error: E): ErrorResponse<ErrorResponseData<E>> => {
+  const response = makeErrorResponse<ErrorResponseData<E>>({})
+  if (!isError(error)) {
+    throw (new TypeError('"error" is expected to be type of "Error".'))
+  }
   response.statusMessage = error.message
   response.data.error = error
   return response
