16418c0ae73f242a43f30f6681b71a65041fbc16
kongxiangyan <kcigaret@outlook.com>
2022-01-09 22:02:18 +0800

    feat(type): add EventHandler to event generics

--- src/ts/@types/event.ts
+++ src/ts/@types/event.ts
@@ -1,2 +1,6 @@
 
+// Q: Why put event related generics in independent @types instead of just put it along with event helpers in external?
+// A: Because the rest part of the codebase other than external will use these generics.
+
 export type SynthesizeEvent<Patch> = Event & { target: Patch }
+export type EventHandler<Target extends EventTarget = EventTarget> = (event: SynthesizeEvent<Target>) => void
