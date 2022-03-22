8c46ce1b323b6b7cdfd28adaa1ed483b1d8ed212
cigaret <kcigaret@outlook.com>
2022-03-22 17:51:41 +0800

    feat: add try catch helper function

--- src/ts/internal/error.ts
+++ src/ts/internal/error.ts
@@ -1,1 +1,8 @@
-export {}
+
+export const tryCatch = <Target extends any>(tryFn: () => Target, catchFn: (exception: unknown) => Target): Target => {
+  try {
+    return tryFn()
+  } catch (exception) {
+    return catchFn(exception)
+  }
+}
