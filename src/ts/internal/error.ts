94f7a5e11e170b335ae2ca0ee425e1f4352551ae
cigaret <kcigaret@outlook.com>
2022-03-22 18:10:28 +0800

    feat: update implementation of tryCatch

--- src/ts/internal/error.ts
+++ src/ts/internal/error.ts
@@ -1,8 +1,21 @@
+import { isUndefined, isFunction } from './base'
 
-export const tryCatch = <Target extends any>(tryFn: () => Target, catchFn: (exception: unknown) => Target): Target => {
+export function tryCatch <Target extends any = any> (tryFn: () => Target, catchFn: ((exception: unknown) => Target)): Target
+export function tryCatch <Target extends any = any> (tryFn: () => Target, catchFn: Target): Target
+export function tryCatch <Target extends any = any> (tryFn: () => Target): Target | undefined
+export function tryCatch (tryFn: () => void): void
+export function tryCatch <Target extends any = any> (
+  tryFn: () => Target, catchFn?: Target | ((exception: unknown) => Target)
+): Target | undefined {
   try {
     return tryFn()
   } catch (exception) {
-    return catchFn(exception)
+    if (!isUndefined(catchFn) && isFunction(catchFn)) {
+      return catchFn(exception)
+    } else if (isUndefined(catchFn)) {
+      return (() => undefined)()
+    } else {
+      return catchFn
+    }
   }
 }
