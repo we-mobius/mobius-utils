81af13902ade549e102feafe19d3bcc6339d9569
kongxiangyan <kcigaret@outlook.com>
2022-03-29 21:53:37 +0800

    fix: fix hybrid helpers according to array helpers changes

--- src/ts/internal/hybrid.ts
+++ src/ts/internal/hybrid.ts
@@ -1,11 +1,5 @@
-import { filter, every, some } from './array'
+import { arrayFilter } from './array'
 import { isTruthy, isFalsy } from './boolean'
 
-import { curry } from '../functional/helpers'
-import { applyTo } from '../functional/combinators'
-
-export const filterTruthy = filter(isTruthy)
-export const filterFalsy = filter(isFalsy)
-
-export const allPass = curry((tests, tar) => every(applyTo(tar), tests))
-export const anyPass = curry((tests, tar) => some(applyTo(tar), tests))
+export const filterTruthy = <T = any>(targetArray: T[]): T[] => arrayFilter(isTruthy, targetArray)
+export const filterFalsy = <T = any>(targetArray: T[]): T[] => arrayFilter(isFalsy, targetArray)
