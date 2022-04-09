78497727ceb0daee26ec03d4548a9f3b5209adb2
kongxiangyan <kcigaret@outlook.com>
2022-04-09 13:31:48 +0800

    refactor(internal/error): refactor tryCatch helper

--- src/ts/internal/error.ts
+++ src/ts/internal/error.ts
@@ -1,21 +1,27 @@
 import { isUndefined, isFunction } from './base'
 
-export function tryCatch <Target extends any = any> (tryer: () => Target, catcher: ((exception: unknown) => Target)): Target
-export function tryCatch <Target extends any = any> (tryer: () => Target, catcher: Target): Target
-export function tryCatch <Target extends any = any> (tryer: () => Target): Target | undefined
+export function tryCatch <Target = any> (tryer: () => Target, catcher: ((exception: unknown) => Target)): Target
+export function tryCatch <Target = any> (tryer: () => Target, catchallValue: Target): Target
+/**
+ * 未提供 `catcher` 或 `catchallValue` 的情况下，若 `tryer` 抛出异常，则返回 `undefined`
+ */
+export function tryCatch <Target = any> (tryer: () => Target): Target | undefined
 export function tryCatch (tryer: () => void): void
-export function tryCatch <Target extends any = any> (
-  tryer: () => Target, catcher?: Target | ((exception: unknown) => Target)
+export function tryCatch <Target = any> (
+  tryer: () => Target, catcherOrCatchallValue?: Target | ((exception: unknown) => Target)
 ): Target | undefined {
   try {
     return tryer()
   } catch (exception) {
-    if (!isUndefined(catcher) && isFunction(catcher)) {
-      return catcher(exception)
-    } else if (isUndefined(catcher)) {
-      return (() => undefined)()
+    // NOTE: intended redundant type guard
+    if (!isUndefined(catcherOrCatchallValue)) {
+      if (isFunction(catcherOrCatchallValue)) {
+        return catcherOrCatchallValue(exception)
+      } else {
+        return catcherOrCatchallValue
+      }
     } else {
-      return catcher
+      return (() => undefined)()
     }
   }
 }
