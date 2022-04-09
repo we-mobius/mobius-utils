af7d918494b7f9b8959acf675532b7e963046ba9
kongxiangyan <kcigaret@outlook.com>
2022-04-09 13:32:35 +0800

    feat(internal/base): improve base predicators

--- src/ts/internal/base.ts
+++ src/ts/internal/base.ts
@@ -9,17 +9,26 @@ import type { Observable } from 'rxjs'
  *             https://tc39.es/ecma262/#sec-typeof-operator
  *   - instanceof: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
  *   - toString: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+ *
+ * - `typeof` operator can operate on non-exist variable.
  */
 
+/**
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#Data_types}
+ */
 export type Primative = string | number | boolean | bigint | symbol | null | undefined
-// @refer https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html#object-type
+/**
+ * @see {@link https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html#object-type}
+ */
 export type NonPrimative = object
 
-// @refer https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#Data_types
 export const isUndefined = (tar: any): tar is undefined =>
   Object.prototype.toString.call(tar) === '[object Undefined]'
-// @refer https://stackoverflow.com/a/52097700
-export const isDefined = <T>(variable: T): variable is T => typeof variable !== 'undefined'
+/**
+ * @see {@link https://stackoverflow.com/a/52097700}
+ */
+export const isDefined = <T>(tar: T): tar is T =>
+  typeof tar !== 'undefined'
 export const isBoolean = (tar: any): tar is boolean =>
   Object.prototype.toString.call(tar) === '[object Boolean]'
 export const isString = (tar: any): tar is string =>
@@ -41,19 +50,34 @@ export const isNonPrimative = (tar: any): tar is NonPrimative => !isPrimative(ta
  *
  * @see {@link isGeneralObject}, {@link isPlainObject}
  */
-export const isObject = (tar: any): tar is Object =>
-  tar instanceof Object
+export const isObject = (tar: any): tar is Object => tar instanceof Object
 /**
  * Predicate whether the target is a general object, use `typeof` operator to check.
  *
- * A general object is an object that is not null, not a function, not a primitive.
- *   Any other object is a general object, an array is a general object, a Map
- *   is a general object, a Set is a general object, etc.
+ * A general object is an object that is:
+ *  - not `null`, not a `function`, not a `primitive`.
+ *
+ * Any other object is a general object:
+ *  - `Array` instance, `Map` instance, `WeakMap` instance, `Set` instance, `Date` instance, etc.
+ *
+ * @code
+ * ```
+ * typeof null // => 'object'
+ * typeof [] // => 'object'
+ * typeof {} // => 'object'
+ * typeof { a: 1 } // => 'object'
+ * typeof (new Map()) // => 'object'
+ * typeof (new WeakMap()) // => 'object'
+ * typeof (new Set()) // => 'object'
+ * typeof (new WeakSet()) // => 'object'
+ * typeof (new Date()) // => 'object'
+ * typeof window // => 'object'
+ * typeof document // => 'object'
+ * ```
  *
  * @see {@link isObject}, {@link isPlainObject}
  */
-export const isGeneralObject = (tar: any): tar is Object =>
-  tar !== null && typeof tar === 'object'
+export const isGeneralObject = (tar: any): tar is Object => tar !== null && typeof tar === 'object'
 
 export interface PlainObject {
   [key: string]: any
@@ -63,10 +87,11 @@ export interface PlainObject {
 /**
  * Predicate whether the target is a plain object.
  *
- * A plain object is an object that is not null, not a function, not a primitive type,
- *   not an array, not a RegExp, not a Date, not a Map, not a Set, not a WeakMap,
- *   not window, not a DOM element, not an Error,
- *   not any other customize object type.
+ * A plain object is an object that is:
+ *  - not `null`, not a `function`, not a `primitive` type,
+ *  - not an `Array`, not a `RegExp`, not a `Date`, not a `Map`, not a `Set`, not a `WeakMap`,
+ *  - not `window`, not a DOM `element`, not an `Error`,
+ *  - not any other customize object type.
  *
  * @see {@link isObject}, {@link isGeneralObject}
  */
@@ -86,8 +111,10 @@ export const isPlainObject = (tar: any): tar is PlainObject => {
   }
   return Object.getPrototypeOf(tar) === proto
 }
-export const isArray = <T>(tar: any): tar is T[] =>
-  Array.isArray(tar)
+/**
+ * @see {@link Array.isArray}
+ */
+export const isArray = <T>(tar: any): tar is T[] => Array.isArray(tar)
 export const isMap = <K, V>(tar: any): tar is Map<K, V> =>
   Object.prototype.toString.call(tar) === '[object Map]'
 export const isWeakMap = <K extends object, V>(tar: any): tar is WeakMap<K, V> =>
@@ -98,14 +125,13 @@ export const isWeakSet = <T extends object>(tar: any): tar is WeakSet<T> =>
   Object.prototype.toString.call(tar) === '[object WeakSet]'
 
 export const isDate = (tar: any): tar is Date =>
-  Boolean(tar) && Object.prototype.toString.call(new Date(tar)) === '[object Date]'
+  Object.prototype.toString.call(tar) === '[object Date]'
 export const isRegExp = (tar: any): tar is RegExp =>
   Object.prototype.toString.call(tar) === '[object RegExp]'
 export const isError = (tar: any): tar is Error =>
   Object.prototype.toString.call(tar) === '[object Error]'
 
-export const isFunction = (tar: any): tar is Function =>
-  typeof tar === 'function'
+export const isFunction = (tar: any): tar is Function => typeof tar === 'function'
 export const isNormalFunction = (tar: any): tar is Function =>
   Object.prototype.toString.call(tar) === '[object Function]'
 export const isAsyncFunction = (tar: any): tar is Function =>
@@ -114,44 +140,93 @@ export const isGeneratorFunction = (tar: any): tar is Function =>
   Object.prototype.toString.call((tar)) === '[object GeneratorFunction]'
 export const isAsyncGeneratorFunction = (tar: any): tar is Function =>
   Object.prototype.toString.call(tar) === '[object AsyncGeneratorFunction]'
+
 export const isPromise = <T>(tar: any): tar is Promise<T> =>
   Object.prototype.toString.call(tar) === '[object Promise]'
 
-export const isEmptyStr = (tar: any): tar is '' =>
-  isString(tar) && tar.length === 0
-export const isEmptyArr = (tar: any): tar is [] =>
-  isArray(tar) && tar.length === 0
-// @refer https://mannes.tech/typescript-object-type-errors/
-export const isEmptyObj = (tar: any): tar is {} =>
-  isPlainObject(tar) && Object.keys(tar).length === 0
-// - `null` and `undefined` are considered empty values
-// - `''` is the empty value for String
-// - `[]` is the empty value for Array
-// - `{}` is the empty value for Object
-export const isEmpty = (tar: any): boolean =>
-  isNull(tar) || isUndefined(tar) || isEmptyStr(tar) || isEmptyArr(tar) || isEmptyObj(tar) ||
+export const isEmptyStr = (tar: any): tar is '' => isString(tar) && tar.length === 0
+export const isEmptyArr = (tar: any): tar is [] => isArray(tar) && tar.length === 0
+/**
+ * @see {@link https://mannes.tech/typescript-object-type-errors/}
+ */
+export const isEmptyObj = (tar: any): tar is {} => isPlainObject(tar) && Object.keys(tar).length === 0
+/**
+ * - `null` and `undefined` are considered empty values
+ * - `''` is the empty value for String
+ * - `[]` is the empty value for Array
+ * - `{}` is the empty value for PlainObject
+ * - for other object types, it will be considered empty
+ *   when the value its `isEmpty` property is true
+ *
+ * @see {@link isNull}, {@link isUndefined}, {@link isEmptyStr}, {@link isEmptyArr}, {@link isEmptyObj}
+ */
+export const isEmpty = (tar: any): boolean => {
+  return isNull(tar) || isUndefined(tar) || isEmptyStr(tar) || isEmptyArr(tar) || isEmptyObj(tar) ||
   (isObject(tar) && tar.isEmpty)
-export const isNil = (v: any): v is (null | undefined) => v === null || v === undefined
-export const isNotNil = (v: any): boolean => !isNil(v)
+}
+
+export const isNil = (tar: any): tar is (null | undefined) => tar === null || tar === undefined
+export const isNotNil = (tar: any): boolean => !isNil(tar)
+
+/**
+ * Check `tar` and `Window` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `Window`.
+ */
 export const isWindow = (tar: any): tar is Window =>
-  Object.prototype.toString.call(tar) === '[object Window]'
-export const isElement = (tar: any): tar is Element => tar instanceof Element
-export const isNode = (tar: any): tar is Node => tar instanceof Node
-export const isHTMLElement = (tar: any): tar is HTMLElement => tar instanceof HTMLElement
+  isGeneralObject(tar) && isDefined(Window) && tar instanceof Window
+/**
+ * Check `tar` and `Element` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `Element`.
+ */
+export const isElement = (tar: any): tar is Element =>
+  isGeneralObject(tar) && isDefined(Element) && tar instanceof Element
+/**
+ * Check `tar` and `Node` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `Node`.
+ */
+export const isNode = (tar: any): tar is Node =>
+  isGeneralObject(tar) && isDefined(Node) && tar instanceof Node
+/**
+ * Check `tar` and `HTMLElement` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `HTMLElement`.
+ */
+export const isHTMLElement = (tar: any): tar is HTMLElement =>
+  isGeneralObject(tar) && isDefined(HTMLElement) && tar instanceof HTMLElement
+/**
+ * Check `tar` and `HTMLCollection` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `HTMLCollection`.
+ */
+export const isHTMLCollection = (tar: any): tar is HTMLCollection =>
+  isGeneralObject(tar) && isDefined(HTMLCollection) && tar instanceof HTMLCollection
+/**
+ * Check `tar` and `Document` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `Document`.
+ */
 export const isDocument = (tar: any): tar is Document =>
-  Object.prototype.toString.call(tar) === '[object HTMLDocument]'
-// @refer https://developer.mozilla.org/zh-CN/docs/Web/API/Event
+  isGeneralObject(tar) && isDefined(Document) && tar instanceof Document
+/**
+ * Check `tar` and `EventTarget` is both defined
+ * before use the `instanceof` operator to check `tar` is instanceof `EventTarget`.
+ *
+ * @see {@link https://developer.mozilla.org/zh-CN/docs/Web/API/Event}
+ */
 export const isEventTarget = (tar: any): tar is EventTarget =>
-  tar instanceof EventTarget
+  isGeneralObject(tar) && isDefined(EventTarget) && tar instanceof EventTarget
+/**
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols#the_iterable_protocol}
+ */
 export const isIterable = <T>(tar: any): tar is Iterable<T> =>
   Object.prototype.toString.call(tar[Symbol.iterator]) === '[object Function]'
+/**
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols#the_iterator_protocol}
+ */
 export const isIterator = <T, TReturn = any, TNext = undefined>(tar: any): tar is Iterator<T, TReturn, TNext> =>
   isObject(tar) && isFunction(tar.next)
 
 export const isObservable = <T>(tar: any): tar is Observable<T> =>
   isObject(tar) && (Boolean(tar.isObservable) || isFunction(tar.subscribe))
 
-export const asIs = <T = any>(v: T, ...args: any[]): T => v
-export const asUndefined = (...v: any[]): undefined => undefined
-export const asNull = (...v: any[]): null => null
-export const asVoid = (...v: any[]): void => { /* do nothing */ }
+export const asIs = <T = any>(tar: T, ...args: any[]): T => tar
+export const asUndefined = (...tar: any[]): undefined => undefined
+export const asNull = (...tar: any[]): null => null
+export const asVoid = (...tar: any[]): void => { /* do nothing */ }
