7ad0b9b3aa346bda2b29bc645ad70645808868f0
kongxiangyan <kcigaret@outlook.com>
2022-06-07 14:25:48 +0800

    feat(external): add EnvironmentVariableManager

--- src/ts/external/environment.ts
+++ src/ts/external/environment.ts
@@ -2,6 +2,8 @@
 
 import { isFunction, isObject } from '../internal/base'
 
+import type { EmptyInterface } from '../@types'
+
 // @refer https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
 
 // wx only exists in miniprogram
@@ -140,5 +142,106 @@ export const adaptMultipleEnvironmentsAsync = async <R = any>(adaptions: Partial
     return await Promise.resolve(forDefault(DEFAULT_ENV_CONTEXTS))
   } else {
     throw (new TypeError('No adaptions found, please provide at least one of "forWeb", "forNode", "forWXMINA", "forDefault".'))
+  }
+}
+
+type ParsersOfVariables<Variables> = {
+  [K in keyof Variables]?: (value: string) => Variables[K]
+}
+interface EnvironmentVariableManagerOptions<Variables> {
+  variables?: Variables
+  parsers?: ParsersOfVariables<Variables>
+}
+const DEFAULT_ENVIRONMENT_VARIABLE_MANAGER_OPTIONS: Required<EnvironmentVariableManagerOptions<any>> = {
+  variables: {},
+  parsers: {}
+}
+/**
+ * 用于以类型安全的方式获取环境变量。
+ *
+ * @example
+ * interface ENV {
+ *   DATABASE: string
+ *   URL: string
+ * }
+ * const appEnvironmentVariableManager = EnvironmentVariableManager.of<ENV>()
+ * const database = appEnvironmentVariableManager.get('DATABASE') // success
+ * const name = appEnvironmentVariableManager.get('NAME') // type check passed
+ * const safeName = appEnvironmentVariableManager.safeGet('NAME') // type check error
+ */
+export class EnvironmentVariableManager<Variables extends EmptyInterface = EmptyInterface> {
+  _variables: Variables
+  _parsers: ParsersOfVariables<Variables>
+
+  constructor (options: EnvironmentVariableManagerOptions<Variables> = DEFAULT_ENVIRONMENT_VARIABLE_MANAGER_OPTIONS) {
+    const { variables, parsers } = {
+      ...DEFAULT_ENVIRONMENT_VARIABLE_MANAGER_OPTIONS as Required<EnvironmentVariableManagerOptions<Variables>>,
+      ...options
+    }
+    this._variables = { ...variables }
+    this._parsers = { ...parsers }
+    this.resetVariables()
+  }
+
+  resetVariables (): void {
+    adaptMultipleEnvironments({
+      forNode: ({ process }) => {
+        const _emptyVariables = {} as any as Variables
+        this._variables = Object.entries(process.env).reduce<Variables>((accumulatedVariables, [key, value]) => {
+          const _key = key as keyof Variables
+          const _value = value as string
+          if (accumulatedVariables[_key] === undefined) {
+            const parser = this._parsers[_key]
+            if (parser === undefined) {
+              accumulatedVariables[_key] = _value as unknown as Variables[keyof Variables]
+            } else {
+              accumulatedVariables[_key] = parser(_value)
+            }
+          }
+          return accumulatedVariables
+        }, _emptyVariables)
+      },
+      forDefault: ({ globalThis }) => {
+        this._variables = (globalThis as any).env ?? {}
+      }
+    })
+  }
+
+  get variables (): Variables { return this._variables }
+
+  static of<Variables extends EmptyInterface = EmptyInterface> (
+    options: EnvironmentVariableManagerOptions<Variables> = DEFAULT_ENVIRONMENT_VARIABLE_MANAGER_OPTIONS
+  ): EnvironmentVariableManager<Variables> {
+    return new EnvironmentVariableManager(options)
+  }
+
+  /**
+   * Get the value of the environment variable.
+   *
+   * @see {@link safeGet}, {@link recklessGet}
+   */
+  get<Key extends keyof Variables> (key: Key): Variables[Key] | undefined {
+    return this._variables[key]
+  }
+
+  /**
+   * Get the value of the environment variable.
+   * @see {@link get}, {@link safeGet}
+   */
+  recklessGet<Key extends keyof Variables> (key: Key): Variables[Key] {
+    return this._variables[key]
+  }
+
+  /**
+   * Get the value of the environment variable.
+   * When the target key is not exist in variables, an error will be thrown.
+   *
+   * @see {@link get}, {@link recklessGet}
+   */
+  safeGet<Key extends keyof Variables> (key: Key): Variables[Key] {
+    if (this._variables[key] === undefined) {
+      throw (new Error(`Environment variable "${key.toString()}" is not defined.`))
+    }
+    return this._variables[key]
   }
 }
