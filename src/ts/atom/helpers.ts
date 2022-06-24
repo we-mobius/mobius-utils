6253b5568ca7f84a40cded42a9111d3cea5bd58d
kongxiangyan <kcigaret@outlook.com>
2022-06-24 11:49:26 +0800

    refactor: update imports

--- src/ts/atom/helpers.ts
+++ src/ts/atom/helpers.ts
@@ -1,12 +1,12 @@
 export * from './helpers/base.helpers'
 
-export * from './helpers/general-create.helpers'
-export * from './helpers/normal-create.helpers'
+export * from './helpers/transform.helpers'
+
+export * from './helpers/general-make.helpers'
+export * from './helpers/normal-make.helpers'
 export * from './helpers/batch-create.helpers'
 export * from './helpers/hybrid-create.helpers'
 export * from './helpers/derive-create.helpers'
-
-export * from './helpers/transform.helpers'
 
 export * from './helpers/normal-link.helpers'
 export * from './helpers/tween-link.helpers'
