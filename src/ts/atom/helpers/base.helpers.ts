5048e73274c3bcbc5cbcd89dad9334be3d1770de
kongxiangyan <kcigaret@outlook.com>
2023-03-12 16:33:40 +0800

    wip: update

--- src/ts/atom/helpers/base.helpers.ts
+++ src/ts/atom/helpers/base.helpers.ts
@@ -5,40 +5,41 @@ import { isMediator } from '../mediators'
 import type { AtomType } from '../atoms'
 
 /**
- * @param tar anything
- * @return { AtomType | undefined } If target is an Atom instance or Mediator instance,
- *                                  return its `atomType`, otherwise return `undefined`.
+ * If target is an Atom instance or Mediator instance, return its `atomType`, otherwise return `undefined`.
+ * @param target anything
  */
-export const getAtomType = (tar: any): AtomType | undefined => tar?.atomType
+export const getAtomType = (target: any): AtomType | undefined => target?.atomType
 
 /**
  * Check whether the given targets are both Atom instance and in same Atom type.
  * @return { boolean } whether the given targets are in same type of Atom.
  */
-export const isSameTypeOfAtom = (tarA: any, tarB: any): boolean => {
-  if (!isAtom(tarA) || !isAtom(tarB)) {
-    return false
-  } else {
-    const atomTypeA = getAtomType(tarA)
-    const atomTypeB = getAtomType(tarB)
-    return atomTypeA === atomTypeB
-  }
+export const isSameTypeOfAtom = (targetA: any, targetB: any): boolean =>
+  isAtom(targetA) && isAtom(targetB) && targetA.atomType !== undefined && targetA.atomType === targetB.atomType
+
+interface IIsSameTypeOfAtom {
+  (targetA: any): ((targetB: any) => boolean)
+  (targetA: any, targetB: any): boolean
 }
-interface IIsSameTypeOfAtom{
-  (tar: any): ((tar: any) => boolean)
-  (tarA: any, tarB: any): boolean
-}
+/**
+ * Curried version of `isSameTypeOfAtom`.
+ * @see {@link isSameTypeOfAtom}
+ */
 export const isSameTypeOfAtom_: IIsSameTypeOfAtom = curry(isSameTypeOfAtom)
 
 /**
  * Check whether the given targets are both Mediator instance and in same Mediator type.
  * @return { boolean } whether the given targets are in same type of Mediator.
  */
-export const isSameTypeOfMediator = (tarA: any, tarB: any): boolean =>
-  isMediator(tarA) && isMediator(tarB) && tarA.mediatorType === tarB.mediatorType
+export const isSameTypeOfMediator = (targetA: any, targetB: any): boolean =>
+  isMediator(targetA) && isMediator(targetB) && targetA.mediatorType !== undefined && targetA.mediatorType === targetB.mediatorType
 
 interface IIsSameTypeOfMediator {
-  (tar: any): ((tar: any) => boolean)
-  (tarA: any, tarB: any): boolean
+  (targetA: any): ((targetB: any) => boolean)
+  (targetA: any, targetB: any): boolean
 }
+/**
+ * Curried version of `isSameTypeOfMediator`.
+ * @see {@link isSameTypeOfMediator}
+ */
 export const isSameTypeOfMediator_: IIsSameTypeOfMediator = curry(isSameTypeOfMediator)
--- src/ts/atom/helpers/batch-create.helpers.ts
+++ src/ts/atom/helpers/batch-create.helpers.ts
@@ -4,77 +4,157 @@ import { isMutator } from '../particles'
 import { Data, Mutation, isData, isMutation, isAtom } from '../atoms'
 import { mutationToDataS, dataToMutationS } from './transform.helpers'
 
+import type { IsArray, IsTuple, ObjectPrettify } from '../../@types/index'
 import type { Mutator } from '../particles'
 import type { DataLike, MutationLike } from '../atoms'
 import type { ReplayDataMediator, ReplayMutationMediator } from '../mediators'
 
-interface BatchCreateOptions {
+interface BatchMakeOptions {
   forceWrap: boolean
 }
-const DEFAULT_BATCH_CREATE_OPTIONS: BatchCreateOptions = {
+const DEFAULT_BATCH_MAKE_OPTIONS: Required<BatchMakeOptions> = {
   forceWrap: false
 }
 
 /**
- * items will be wrapped in Data.
+ * Items will be wrapped in `Data`.
  */
-const forceWrapToData = <V>(item: V): Data<V> => Data.of(item)
-
-interface IWrapToData {
-  <V>(item: Data<V>): Data<V>
-  <V>(item: ReplayDataMediator<V>): ReplayDataMediator<V>
-  <P, C>(item: Mutation<P, C>): Data<C>
-  <P, C>(item: ReplayMutationMediator<P, C>): ReplayDataMediator<C>
-  <V>(item: V): Data<V>
+function forceWrapToData <V = any> (target: V): Data<V> {
+  return Data.of(target)
 }
+
+type WrapToData<Target = any> = Target extends Data<infer V>
+  ? Data<V>
+  : Target extends ReplayDataMediator<infer V>
+    ? ReplayDataMediator<V>
+    : Target extends Mutation<any, infer C>
+      ? Data<C>
+      : Target extends ReplayMutationMediator<any, infer C>
+        ? ReplayDataMediator<C>
+        : Data<Target>
 /**
  * Mutations will be transform to Data, Datas will not be handled, other values will be wrapped in Data.
  */
-const wrapToData: IWrapToData = (item: any): any => isData(item) ? item : (isMutation(item) ? mutationToDataS(item) : Data.of(item))
-
-/**
- * forceWrap - true: items will be wrapped in Data.
- *
- * forceWrap - false: Mutations will be transform to Data, Datas will not be handled, other values will be wrapped in Data.
- */
-export const createDataInArray = <V>(
-  arr: V[], options: BatchCreateOptions = DEFAULT_BATCH_CREATE_OPTIONS
-): Array<DataLike<any>> => {
-  if (!isArray(arr)) {
-    throw new TypeError('"arr" is expected to be type of "Array".')
-  }
-
-  const { forceWrap } = { ...DEFAULT_BATCH_CREATE_OPTIONS, ...options }
-
-  const wrapFn = forceWrap ? forceWrapToData : wrapToData
-  return arr.map(wrapFn)
+function wrapToData <V = any> (target: Data<V>): Data<V>
+function wrapToData <V = any> (target: ReplayDataMediator<V>): ReplayDataMediator<V>
+function wrapToData <P = any, C = any> (target: Mutation<P, C>): Data<C>
+function wrapToData <P = any, C = any> (target: ReplayMutationMediator<P, C>): ReplayDataMediator<C>
+function wrapToData <V = any> (target: V): Data<V>
+function wrapToData (target: any): any {
+  return isData(target) ? target : (isMutation(target) ? mutationToDataS(target) : Data.of(target))
 }
-export const createDataInArray_ = looseCurryN(1, createDataInArray)
 
+export type WrapDataInArray<T extends readonly any[]> = IsArray<T> extends true
+  // check whether the array is readonly
+  ? T extends any[]
+    ? (T extends Array<infer U> ? Array<WrapToData<U>> : never)
+    : (T extends ReadonlyArray<infer U> ? ReadonlyArray<WrapToData<U>> : never)
+  : IsTuple<T> extends true
+    // check whether the tuple is readonly
+    ? T extends any[]
+      ? T extends [infer First, ...infer Rest] ? [WrapToData<First>, ...WrapDataInArray<Rest>] : []
+      : T extends readonly [infer First, ...infer Rest] ? readonly [WrapToData<First>, ...WrapDataInArray<Rest>] : readonly []
+    : never
+export type ForceWrapDataInArray<T extends readonly any[]> = IsArray<T> extends true
+  // check whether the array is readonly
+  ? T extends any[]
+    ? (T extends Array<infer U> ? Array<Data<U>> : never)
+    : (T extends ReadonlyArray<infer U> ? ReadonlyArray<Data<U>> : never)
+  : IsTuple<T> extends true
+    // check whether the tuple is readonly
+    ? T extends any[]
+      ? T extends [infer First, ...infer Rest] ? [Data<First>, ...ForceWrapDataInArray<Rest>] : []
+      : T extends readonly [infer First, ...infer Rest] ? readonly [Data<First>, ...ForceWrapDataInArray<Rest>] : readonly []
+    : never
 /**
- * forceWrap - true: items will be wrapped in Data.
+ * forceWrap - true: items will be wrapped in Data. See {@link forceWrapToData}.
  *
- * forceWrap - false: Mutations will be transform to Data, Datas will not be handled, other values will be wrapped in Data.
+ * forceWrap - false: Mutations will be transform to Data, Datas will not be handled,
+ *                    other values will be wrapped in Data. See {@link wrapToData}.
+ * @example
+ * ```
+ *   const tupleA: [number, Data<number>, number] = [1, Data.of(2), 3]
+ *   const arrayA = [1, 2, 3]
+ *   // Expect: [Data<number>, Data<number>, Data<number>]
+ *   const dataInTupleA = makeDataInArray(tupleA)
+ *   // Expect: Data<number>[]
+ *   const dataInArrayA = makeDataInArray(arrayA)
+ * ```
  */
-export const createDataInObject = <V>(
-  obj: Record<string, V>, options: BatchCreateOptions = DEFAULT_BATCH_CREATE_OPTIONS
-): Record<string, DataLike<any>> => {
-  if (!isPlainObject(obj)) {
-    throw new TypeError('"obj" is expected to be type of "PlainObjcet".')
+export function makeDataInArray <V extends readonly any[] = any> (
+  targets: V, options?: BatchMakeOptions & { forceWrap: false }
+): WrapDataInArray<V>
+export function makeDataInArray <V extends readonly any[] = any> (
+  targets: V, options?: BatchMakeOptions & { forceWrap: true }
+): ForceWrapDataInArray<V>
+export function makeDataInArray <V extends readonly any[] = any> (
+  targets: V, options: BatchMakeOptions = DEFAULT_BATCH_MAKE_OPTIONS
+): Array<DataLike<any>> {
+  if (!isArray(targets)) {
+    throw new TypeError('"targets" is expected to be type of "Array".')
   }
 
-  const { forceWrap } = { ...DEFAULT_BATCH_CREATE_OPTIONS, ...options }
-
-  const result: Record<string, any> = {}
+  const preparedOptions = { ...DEFAULT_BATCH_MAKE_OPTIONS, ...options }
+  const { forceWrap } = preparedOptions
 
   const wrapFn = forceWrap ? forceWrapToData : wrapToData
-  Object.entries(obj).forEach(([key, value]) => {
+  return targets.map(wrapFn)
+}
+/**
+ * Curried version of {@link makeDataInArray}.
+ */
+export const makeDataInArray_ = looseCurryN(1, makeDataInArray)
+
+type WrapDataInObject<T extends Record<any, any>> = ObjectPrettify<{
+  [Key in keyof T]: WrapToData<T[Key]>
+}>
+type ForceWrapDataInObject<T extends Record<any, any>> = ObjectPrettify<{
+  [Key in keyof T]: Data<T[Key]>
+}>
+/**
+ * forceWrap - true: items will be wrapped in Data. See {@link forceWrapToData}.
+ *
+ * forceWrap - false: Mutations will be transform to Data, Datas will not be handled,
+ *                    other values will be wrapped in Data. See {@link wrapToData}.
+ * @example
+ * ```
+ *   const indexedObject: { [key: string]: number } = { a: 1, b: 2, c: 3 }
+ *   // Expect: Record<string, Data<number>>
+ *   const dataInIndexedObject = makeDataInObject(indexedObject)
+ *   const namedObject = { a: 1, b: Data.of(2), c: 3 }
+ *   // Expect: { a: Data<number>, b: Data<number>, c: Data<number> }
+ *   const dataInNamedObject = makeDataInObject(namedObject)
+ * ```
+ */
+export function makeDataInObject <V extends Record<any, any>> (
+  targets: V, options?: BatchMakeOptions & { forceWrap: false }
+): WrapDataInObject<V>
+export function makeDataInObject <V extends Record<any, any>> (
+  targets: V, options?: BatchMakeOptions & { forceWrap: true }
+): ForceWrapDataInObject<V>
+export function makeDataInObject <V extends Record<any, any> = any> (
+  targets: Record<string, V>, options: BatchMakeOptions = DEFAULT_BATCH_MAKE_OPTIONS
+): Record<string, DataLike<any>> {
+  if (!isPlainObject(targets)) {
+    throw new TypeError('"targets" is expected to be type of "PlainObject".')
+  }
+
+  const preparedOptions = { ...DEFAULT_BATCH_MAKE_OPTIONS, ...options }
+  const { forceWrap } = preparedOptions
+
+  const result: Record<string, any> = { }
+
+  const wrapFn = forceWrap ? forceWrapToData : wrapToData
+  Object.entries(targets).forEach(([key, value]) => {
     result[key] = wrapFn(value)
   })
 
   return result
 }
-export const createDataInObject_ = looseCurryN(1, createDataInObject)
+/**
+ * Curried version of {@link makeDataInObject}.
+ */
+export const makeDataInObject_ = looseCurryN(1, makeDataInObject)
 
 /**
  * Datas will be transform to Mutation, Mutations will not be handled, other values will be wrapped in Mutation.
@@ -113,12 +193,12 @@ const wrapToMutation: IWrapToMutation = (item: any): any => {
  * forceWrap - false: Datas will be transform to Mutation, Mutations will not be handled, other values will be wrapped in Mutation.
  */
 export const createMutationInArray = <V>(
-  arr: V[], options: BatchCreateOptions = DEFAULT_BATCH_CREATE_OPTIONS
+  arr: V[], options: BatchMakeOptions = DEFAULT_BATCH_MAKE_OPTIONS
 ): Array<MutationLike<any, any>> => {
   if (!isArray(arr)) {
     throw new TypeError('"arr" is expected to be type of "Array".')
   }
-  const { forceWrap } = { ...DEFAULT_BATCH_CREATE_OPTIONS, ...options }
+  const { forceWrap } = { ...DEFAULT_BATCH_MAKE_OPTIONS, ...options }
 
   const wrapFn = forceWrap ? forceWrapToMutation : wrapToMutation
   return arr.map(wrapFn)
@@ -131,13 +211,13 @@ export const createMutationInArray_ = looseCurryN(1, createMutationInArray)
  * forceWrap - false: Datas will be transform to Mutation, Mutations will not be handled, other values will be wrapped in Mutation.
  */
 export const createMutationInObject = <V>(
-  obj: Record<string, V>, options: BatchCreateOptions = DEFAULT_BATCH_CREATE_OPTIONS
+  obj: Record<string, V>, options: BatchMakeOptions = DEFAULT_BATCH_MAKE_OPTIONS
 ): Record<string, MutationLike<any, any>> => {
   if (!isPlainObject(obj)) {
     throw new TypeError('"obj" is expected to be type of "PlainObjcet"')
   }
 
-  const { forceWrap } = { ...DEFAULT_BATCH_CREATE_OPTIONS, ...options }
+  const { forceWrap } = { ...DEFAULT_BATCH_MAKE_OPTIONS, ...options }
 
   const result: Record<string, any> = {}
 
@@ -185,13 +265,13 @@ const wrapToAtom: IWrapToAtom = (item: any): any => {
  * forceWrap - false: Atoms will not be handled, functions will be wrapped in Mutation, other values will be wrapped in Data.
  */
 export const createAtomInArray = <V>(
-  arr: V[], options: BatchCreateOptions = DEFAULT_BATCH_CREATE_OPTIONS
+  arr: V[], options: BatchMakeOptions = DEFAULT_BATCH_MAKE_OPTIONS
 ): Array<DataLike<any> | MutationLike<any, any>> => {
   if (!isArray(arr)) {
     throw new TypeError('"arr" is expected to be type of "Array".')
   }
 
-  const { forceWrap } = { ...DEFAULT_BATCH_CREATE_OPTIONS, ...options }
+  const { forceWrap } = { ...DEFAULT_BATCH_MAKE_OPTIONS, ...options }
 
   const wrapFn = forceWrap ? forceWrapToAtom : wrapToAtom
   return arr.map(wrapFn)
@@ -204,7 +284,7 @@ export const createAtomInArray_ = looseCurryN(1, createAtomInArray)
  * forceWrap - false: Atoms will not be handled, functions will be wrapped in Mutation, other values will be wrapped in Data.
  */
 export const createAtomInObject = <V>(
-  obj: Record<string, V>, options: BatchCreateOptions = DEFAULT_BATCH_CREATE_OPTIONS
+  obj: Record<string, V>, options: BatchMakeOptions = DEFAULT_BATCH_MAKE_OPTIONS
 ): Record<string, DataLike<any> | MutationLike<any, any>> => {
   if (!isPlainObject(obj)) {
     throw new TypeError('"obj" is expected to be type of "PlainObject".')
